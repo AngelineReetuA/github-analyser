5ba4a9fd323b757c4a75b223356a37fe83a192cf
Angeline Reetu <angeline.reetu@sysvine.com>
2024-07-15 16:17:15 +0530

    moved the NavBar here

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
 import InitialAnalysis from "./pages/InitialAnalysis";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import CodeIcon from "@mui/icons-material/Code";
+import PersonIcon from "@mui/icons-material/Person";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
+import { Box, Tabs, Tab } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  const handleTabChange = (e, tabIndex) => {
+    setCurrentTabIndex(tabIndex);
+  };
   return (
     <>
       <Header />
-      <div className="d-flex">
-        <NavBar />
-        <InitialAnalysis />
-      </div>
+      <Box sx={{ display: "flex" }}>
+        <Box
+          sx={{
+            flexGrow: 1,
+            bgcolor: "#d9edf8",
+            display: "flex",
+          }}
+        >
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={currentTabIndex}
+            onChange={handleTabChange}
+            sx={{ borderRight: 1, borderColor: "#7eb8d9" }}
+          >
+            <Tab icon={<RemoveRedEyeIcon />} />
+            <Tab icon={<CodeIcon />} />
+            <Tab icon={<PersonIcon />} />
+          </Tabs>
+        </Box>
+        {currentTabIndex === 0 && <InitialAnalysis />}
+      </Box>
     </>
   );
 }
