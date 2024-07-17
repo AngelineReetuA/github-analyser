b4ce16157b0438fd15759df25b43696bf5e0146d
Angeline Reetu <angeline.reetu@sysvine.com>
2024-07-17 16:50:01 +0530

    moved schema seperately

---
+++ src/pages/SchemaValidation.jsx
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const schema = yup.object().shape({
+    name: yup
+      .string()
+      .required("Please enter your name")
+      .matches(/^[a-zA-Z'-\s]+$/, "Invalid name")
+      .min(2),
+    email: yup.string().email("Is this really an email?").required(),
+    phone: yup
+      .string()
+      .required("Please enter a phone number")
+      .min(10,"Phone number has to be 10 digits long").max(10,"Phone number should not be longer than 10 digits")
+      .matches(/^[0-9]+$/, "Must be only digits"),
+    gender: yup.string().required(),
+    reason: yup.string().required("Please select the reason for connection"),
+    message: yup
+      .string()
+      .nullable()
+      .notRequired()
+      .max(40, "Keep your message less than 40 words"),
+    rating: yup.number().nullable().notRequired(),
+  });
