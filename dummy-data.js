3c60684ea6bb5a416b5c176c71149830b9ff50a4
Angeline Reetu <angeline.reetu@sysvine.com>
2024-08-13 14:52:39 +0530

    seperated data into a new file

---
+++ dummy-data.js
@@ -0,0 +1,136 @@
+// need the repo owner name on the below repoLink
+// supposed to be `https://www.github.com/${ownerName}/${this.repoName}`
+
+const data = [
+  {
+    id: "1",
+    repoName: "mental_health_chatbot",
+    repoDesc:
+      "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+    repoLoc: 4567,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "2",
+    repoName: "carpoolingDApp",
+    repoDesc:
+      "this is a really cool description of a really cool project with complete innovation at its finest",
+    repoLoc: 12567,
+    repoLang: "Hyperledger Fabric, Node, React",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "3",
+    repoName: "wordle",
+    repoDesc:
+      "this is a really cool description of a really cool project with cute puns",
+    repoLoc: 632,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "4",
+    repoName: "rumour-spreading-algorithm",
+    repoDesc: "this is a really cool description of a really cool project",
+    repoLoc: 89,
+    repoLang: "Node, Javascript",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "5",
+    repoName: "mental_health_chatbot",
+    repoDesc:
+      "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+    repoLoc: 4567,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "6",
+    repoName: "carpoolingDApp",
+    repoDesc:
+      "this is a really cool description of a really cool project with complete innovation at its finest",
+    repoLoc: 12567,
+    repoLang: "Hyperledger Fabric, Node, React",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "7",
+    repoName: "wordle",
+    repoDesc:
+      "this is a really cool description of a really cool project with cute puns",
+    repoLoc: 632,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "8",
+    repoName: "rumour-spreading-algorithm",
+    repoDesc: "this is a really cool description of a really cool project",
+    repoLoc: 89,
+    repoLang: "Node, Javascript",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "9",
+    repoName: "mental_health_chatbot",
+    repoDesc:
+      "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+    repoLoc: 4567,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "10",
+    repoName: "carpoolingDApp",
+    repoDesc:
+      "this is a really cool description of a really cool project with complete innovation at its finest",
+    repoLoc: 12567,
+    repoLang: "Hyperledger Fabric, Node, React",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "11",
+    repoName: "wordle",
+    repoDesc:
+      "this is a really cool description of a really cool project with cute puns",
+    repoLoc: 632,
+    repoLang: "Javascript, HTML, CSS",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+  {
+    id: "12",
+    repoName: "rumour-spreading-algorithm",
+    repoDesc: "this is a really cool description of a really cool project",
+    repoLoc: 89,
+    repoLang: "Node, Javascript",
+    repoLink: function () {
+      return `https://www.github.com/${this.repoName}`;
+    },
+  },
+];
+
+export default data;
--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -2,6 +2,7 @@ import { Link, Typography } from "@mui/material";
 import { Container } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { DataGrid } from "@mui/x-data-grid";
+import data from "../../dummy-data";
 
 export default function Repositories() {
   const columns = [
@@ -23,114 +24,112 @@ export default function Repositories() {
       },
     },
   ];
-
-  const data = [
-    {
-      id: "1",
-      repoName: "mental_health_chatbot",
-      repoDesc:
-        "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
-      repoLoc: 4567,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "2",
-      repoName: "carpoolingDApp",
-      repoDesc:
-        "this is a really cool description of a really cool project with complete innovation at its finest",
-      repoLoc: 12567,
-      repoLang: "Hyperledger Fabric, Node, React",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "3",
-      repoName: "wordle",
-      repoDesc:
-        "this is a really cool description of a really cool project with cute puns",
-      repoLoc: 632,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "4",
-      repoName: "rumour-spreading-algorithm",
-      repoDesc: "this is a really cool description of a really cool project",
-      repoLoc: 89,
-      repoLang: "Node, Javascript",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "5",
-      repoName: "mental_health_chatbot",
-      repoDesc:
-        "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
-      repoLoc: 4567,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "6",
-      repoName: "carpoolingDApp",
-      repoDesc:
-        "this is a really cool description of a really cool project with complete innovation at its finest",
-      repoLoc: 12567,
-      repoLang: "Hyperledger Fabric, Node, React",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "7",
-      repoName: "wordle",
-      repoDesc:
-        "this is a really cool description of a really cool project with cute puns",
-      repoLoc: 632,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "8",
-      repoName: "rumour-spreading-algorithm",
-      repoDesc: "this is a really cool description of a really cool project",
-      repoLoc: 89,
-      repoLang: "Node, Javascript",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "9",
-      repoName: "mental_health_chatbot",
-      repoDesc:
-        "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
-      repoLoc: 4567,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "10",
-      repoName: "carpoolingDApp",
-      repoDesc:
-        "this is a really cool description of a really cool project with complete innovation at its finest",
-      repoLoc: 12567,
-      repoLang: "Hyperledger Fabric, Node, React",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "11",
-      repoName: "wordle",
-      repoDesc:
-        "this is a really cool description of a really cool project with cute puns",
-      repoLoc: 632,
-      repoLang: "Javascript, HTML, CSS",
-      repoLink: `https://www.github.com`,
-    },
-    {
-      id: "12",
-      repoName: "rumour-spreading-algorithm",
-      repoDesc: "this is a really cool description of a really cool project",
-      repoLoc: 89,
-      repoLang: "Node, Javascript",
-      repoLink: `https://www.github.com`,
-    },
-  ];
+  //   {
+  //     id: "1",
+  //     repoName: "mental_health_chatbot",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+  //     repoLoc: 4567,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "2",
+  //     repoName: "carpoolingDApp",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with complete innovation at its finest",
+  //     repoLoc: 12567,
+  //     repoLang: "Hyperledger Fabric, Node, React",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "3",
+  //     repoName: "wordle",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with cute puns",
+  //     repoLoc: 632,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "4",
+  //     repoName: "rumour-spreading-algorithm",
+  //     repoDesc: "this is a really cool description of a really cool project",
+  //     repoLoc: 89,
+  //     repoLang: "Node, Javascript",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "5",
+  //     repoName: "mental_health_chatbot",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+  //     repoLoc: 4567,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "6",
+  //     repoName: "carpoolingDApp",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with complete innovation at its finest",
+  //     repoLoc: 12567,
+  //     repoLang: "Hyperledger Fabric, Node, React",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "7",
+  //     repoName: "wordle",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with cute puns",
+  //     repoLoc: 632,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "8",
+  //     repoName: "rumour-spreading-algorithm",
+  //     repoDesc: "this is a really cool description of a really cool project",
+  //     repoLoc: 89,
+  //     repoLang: "Node, Javascript",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "9",
+  //     repoName: "mental_health_chatbot",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with a lot of buzzwords and a really cool sounding brainy paragraph",
+  //     repoLoc: 4567,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "10",
+  //     repoName: "carpoolingDApp",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with complete innovation at its finest",
+  //     repoLoc: 12567,
+  //     repoLang: "Hyperledger Fabric, Node, React",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "11",
+  //     repoName: "wordle",
+  //     repoDesc:
+  //       "this is a really cool description of a really cool project with cute puns",
+  //     repoLoc: 632,
+  //     repoLang: "Javascript, HTML, CSS",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  //   {
+  //     id: "12",
+  //     repoName: "rumour-spreading-algorithm",
+  //     repoDesc: "this is a really cool description of a really cool project",
+  //     repoLoc: 89,
+  //     repoLang: "Node, Javascript",
+  //     repoLink: `https://www.github.com`,
+  //   },
+  // ];
 
   return (
     <>
