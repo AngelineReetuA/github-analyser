39bcf223ad8061ec738bd2db05830d57161f80fe
Angeline Reetu <angeline.reetu@sysvine.com>
2024-08-16 11:14:09 +0530

    made the header visible only after first page entry

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import Header from "./components/Header";
 import NavBar from "./components/Drawer.jsx";
 import FirstPage from "./pages/FirstPage.jsx";
 import { Routes, Route } from "react-router-dom";
-import { Box, Container } from "@mui/material";
+import { Box } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
-      <Header />
-      <NavBar />
+      {location.pathname != "/" && (
+        <>
+          <Header />
+          <NavBar />
+        </>
+      )}
       <Box sx={{ marginTop: "60px", marginLeft: "90px" }}>
         <Routes>
-          <Route path="" element={<FirstPage />} />
+          <Route path="/" element={<FirstPage />} />
           <Route path="/user/user-analysis" element={<InitialAnalysis />} />
           <Route
             path="/user/code-analysis/repositories"
@@ -25,10 +32,18 @@ function App() {
             element={<CodeAnalysis />}
           />
           <Route path="/user/contact" element={<Contact />} />
+          <Route
+            path="/user/contact"
+            element={
+              <center>
+                <h4>Page not found</h4>
+              </center>
+            }
+          />
         </Routes>
       </Box>
     </>
-  )
+  );
 }
 
 export default App;
