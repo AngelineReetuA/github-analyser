610967d5186530e19fb8720dcb4f7eb9b73971f3
Angeline Reetu <angeline.reetu@sysvine.com>
2024-09-28 21:18:45 +0530

    added close button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,43 @@
-import { AppBar, Typography, Box } from "@mui/material";
+import { AppBar, Typography, Toolbar, Box } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  function handleClose() {
+    Swal.fire({
+      title: "Close this profile?",
+      text: "You will go back to the homepage",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#98c1d9",
+      cancelButtonColor: "#ee6c4d",
+      confirmButtonText: "Yes, go back!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate("/");
+      }
+    });
+  }
+
   return (
     <>
-      <AppBar
-        position="fixed"
-        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, height:"60px" }}
-      >
-        <Typography
-          variant="h6"
-          component="div"
-          sx={{ flexGrow: 1, bgcolor: "#293241", padding: "15px" }}
-        >
-          Quick Github Analyser
-        </Typography>
+      <AppBar position="fixed" sx={{ height: "60px", bgcolor: "#293241" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" component="div" sx={{ bgcolor: "#293241" }}>
+            Quick Github Analyser
+          </Typography>
+          <CloseIcon
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ fontSize: "30px", ":hover": { backgroundColor: "#e0fbfc", color:"#293241" } }}
+            onClick={handleClose}
+          />
+        </Toolbar>
       </AppBar>
     </>
   );
