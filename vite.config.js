65c47f5e16e0636893c0c939f6de7d7b54b7c6e9
Angeline Reetu A <angelinereetu@gmail.com>
2024-10-02 15:56:15 +0530

    added comma

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-analyser/'
+  base: '/github-analyser/',
   plugins: [react()],
 })
